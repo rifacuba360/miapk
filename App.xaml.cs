b455ef86e0f02c29b486c637363f58690e76ae30
satoshi Nakamoto <rifacuba360@gmail.com>
2025-11-13 05:01:26 -0500

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+namespace rifa3602
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new AppShell();
+        }
+    }
+}
